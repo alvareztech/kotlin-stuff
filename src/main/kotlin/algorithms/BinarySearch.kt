bf12d961dd8c1f2537ad170b896e9be728fa6fc8
Daniel Alvarez <daniel@alvarez.tech>
2022-10-23 13:18:44 -0700

    Base project

---
+++ src/main/kotlin/algorithms/BinarySearch.kt
@@ -0,0 +1,11 @@
+package algorithms
+
+fun binarySearch(numbers: IntArray, target: Int): Int {
+    var index = 0
+    var r = numbers.size
+    while (index < r) {
+        val middle = (index + r) / 2
+        if (numbers[middle] < target) index = middle + 1 else r = middle
+    }
+    return index
+}
